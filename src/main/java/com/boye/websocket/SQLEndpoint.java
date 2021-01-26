00e785f26ec72dd9c72b24093c395fd85e6ba04f
aaiit <abdellatif.aithammadi@gmail.com>
2021-01-26 21:26:28 +0100

    v*

--- src/main/java/com/boye/websocket/SQLEndpoint.java
+++ src/main/java/com/boye/websocket/SQLEndpoint.java
@@ -58,3 +58,16 @@ public class SQLEndpoint {
         System.out.println("Session " + session.getId() + " is closed.");
     }
 }
+
+
+private Connection getConnection() throws URISyntaxException, SQLException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        int port = dbUri.getPort();
+
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + port + dbUri.getPath();
+
+        return DriverManager.getConnection(dbUrl, username, password);
+    }
