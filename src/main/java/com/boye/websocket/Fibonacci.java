a6b3ce4393dd0adfc41df0c44e4bd9ddf61aaa1f
aaiit <abdellatif.aithammadi@gmail.com>
2021-01-29 12:10:31 +0100

    RMI

---
+++ src/main/java/com/boye/websocket/Fibonacci.java
@@ -0,0 +1,17 @@
+package com.boye.websocket;
+
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+
+
+public  class Fibonacci extends UnicastRemoteObject  {
+    public Fibonacci() throws RemoteException {
+    }
+
+    public long fibonacci(int rank) throws RemoteException {
+
+        return 2020;
+    }
+}
--- src/main/java/com/boye/websocket/Main.java
+++ src/main/java/com/boye/websocket/Main.java
@@ -28,27 +28,14 @@ import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 
 
-import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
-
-
-
-private  class Fibonacci extends UnicastRemoteObject  {
-    public Fibonacci() throws RemoteException {
-    }
-
-    public long fibonacci(int rank) throws RemoteException {
-
-        return 2020;
-    }
-}
 
 public class Main {
 
 
+    Fibonacci f = new Fibonacci();
+
     public static void main(String[] a) {
         try {
-            Fibonacci f = new Fibonacci();
             LocateRegistry.createRegistry(java.lang.Integer.parseInt(System.getenv("PORT")));
             Naming.rebind("oktest" , f);
             System.out.println("Server Ready...");
