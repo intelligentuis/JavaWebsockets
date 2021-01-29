34566c1476c126cee23e5b2312baef554fb23dbc
aaiit <abdellatif.aithammadi@gmail.com>
2021-01-29 11:52:45 +0100

    RMI

--- src/main/java/com/boye/websocket/Main.java
+++ src/main/java/com/boye/websocket/Main.java
@@ -28,6 +28,20 @@ import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 
 
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+
+
+public class Fibonacci extends UnicastRemoteObject  {
+    public Fibonacci() throws RemoteException {
+    }
+
+    public long fibonacci(int rank) throws RemoteException {
+
+        return 2020;
+    }
+}
 
 public class Main {
 
