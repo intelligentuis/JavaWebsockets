7b5bf4d89d74dc9fd962f2a76dd0e7757a2c9379
aaiit <abdellatif.aithammadi@gmail.com>
2021-01-29 12:26:32 +0100

    RMI

--- src/main/java/com/boye/websocket/Main.java
+++ src/main/java/com/boye/websocket/Main.java
@@ -33,8 +33,8 @@ public class Main {
 
 
 
-    public static void main(String[] a) throws RemoteException {
-        static Fibonacci f = new Fibonacci();
+    static Fibonacci f = new Fibonacci();
+    public static void main(String[] a){
         
         try {
             LocateRegistry.createRegistry(java.lang.Integer.parseInt(System.getenv("PORT")));
